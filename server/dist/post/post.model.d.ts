4fb23b239dd6845403a329c58295c6580128b114
Omer Gaizinger <omerg863@gmail.com>
2022-10-04 21:24:38 +0300

    removed modules

--- server/dist/post/post.model.d.ts
+++
@@ -1,8 +0,0 @@
-import * as mongoose from "mongoose";
-export declare const PostScheme: mongoose.Schema<Post, mongoose.Model<Post, any, any, any, any>, {}, {}, {}, {}, "type", Post>;
-export interface Post {
-    title: string;
-    userId: mongoose.Schema.Types.ObjectId;
-    content: string;
-    date: Date;
-}
--- server/dist/schedule/schedule.model.d.ts
+++
@@ -1,8 +0,0 @@
-import * as mongoose from "mongoose";
-export declare const ScheduleScheme: mongoose.Schema<Schedule, mongoose.Model<Schedule, any, any, any, any>, {}, {}, {}, {}, "type", Schedule>;
-export interface Schedule {
-    date: Date;
-    num_weeks: number;
-    weeks: Map<String, Object>;
-    publish: boolean;
-}
