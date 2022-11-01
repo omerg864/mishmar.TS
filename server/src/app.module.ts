939f1d4d66462b25fabd4fc0cfd1134c6efeb831
omerg864 <omerg863@gmail.com>
2022-11-01 16:37:48 +0200

    vercel deployment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -9,13 +9,10 @@ import { StructureModule } from './structure/structure.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { LogModule } from './log/log.module';
 
 @Module({
-  imports: [ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '..', '..', 'client', 'build'),
-  }) ,UserModule, ScheduleModule, ShiftModule, EventModule, PostModule, SettingsModule, StructureModule, LogModule, ConfigModule.forRoot() , MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.wkjalhp.mongodb.net/${process.env.DB_TYPE}?retryWrites=true&w=majority`)],
+  imports :[UserModule, ScheduleModule, ShiftModule, EventModule, PostModule, SettingsModule, StructureModule, LogModule, ConfigModule.forRoot() , MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.wkjalhp.mongodb.net/${process.env.DB_TYPE}?retryWrites=true&w=majority`)],
   controllers: [],
   providers: [],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import express from 'express';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
      cors: true 
     });
+  app.use(express.static(join(__dirname, "../../client/build")))
   await app.listen(process.env.PORT || 3000, () => {
     if (process.env.NODE_ENV === 'production') {
       console.log(`Server is running on production mode`);
