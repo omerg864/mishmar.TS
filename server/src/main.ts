b724b5798dc2a110f3beee7cee0eda5f5c945408
omerg864 <omerg863@gmail.com>
2022-11-01 16:26:07 +0200

    vercel deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import express from 'express';
-import { join } from 'path';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
      cors: true 
     });
-    if (process.env.NODE_ENV === 'production') {
-      app.use(express.static(join(__dirname, '..', '..', 'client', 'build')))
-    }
   await app.listen(process.env.PORT || 3000, () => {
     if (process.env.NODE_ENV === 'production') {
       console.log(`Server is running on production mode`);
