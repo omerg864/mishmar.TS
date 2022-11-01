926549c4335d8ac46447f96e2baa57cb389d6e46
omerg864 <omerg863@gmail.com>
2022-11-01 14:32:37 +0200

    build fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import express from 'express';
+import { join } from 'path';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
      cors: true 
     });
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(join(__dirname, '..', '..', 'client', 'build')))
+    }
   await app.listen(process.env.PORT, () => {
     if (process.env.NODE_ENV === 'production') {
       console.log(`Server is running on production mode`);
