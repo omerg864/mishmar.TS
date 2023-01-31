61e52e3774d21fd57d809e659b781d89955e5a0e
Omer <omerg863@gmail.com>
2023-01-31 13:51:52 +0200

    :sparkles: chnage db port

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -15,21 +15,36 @@ import * as path from 'path';
 import * as dotenv from 'dotenv';
 
 if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({
-    path: path.resolve(__dirname, '../.env')
-  })
+    dotenv.config({
+        path: path.resolve(__dirname, '../.env'),
+    });
 }
 
-const imports = [UserModule, ScheduleModule, ShiftModule, EventModule, PostModule, SettingsModule, StructureModule, LogModule, ConfigModule.forRoot() , MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.wkjalhp.mongodb.net/${process.env.DB_TYPE}?retryWrites=true&w=majority`)]
+const imports = [
+    UserModule,
+    ScheduleModule,
+    ShiftModule,
+    EventModule,
+    PostModule,
+    SettingsModule,
+    StructureModule,
+    LogModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.${process.env.DB_PORT}.mongodb.net/${process.env.DB_TYPE}?retryWrites=true&w=majority`
+    ),
+];
 
 if (process.env.NODE_ENV === 'production') {
-  imports.push(ServeStaticModule.forRoot({
-    rootPath: path.join(__dirname, "../../client/build")
-  }))
+    imports.push(
+        ServeStaticModule.forRoot({
+            rootPath: path.join(__dirname, '../../client/build'),
+        })
+    );
 }
 @Module({
-  imports,
-  controllers: [],
-  providers: [],
+    imports,
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
