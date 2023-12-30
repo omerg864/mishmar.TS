d3b7b891c042f612c5fce7cc7be38b4d0fa1fde8
Omer Gaiznger <Omerg863@gmail.com>
2023-12-30 22:33:31 +0200

    pwa support

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,21 +1,21 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import ExpressMongoSanitize from 'express-mongo-sanitize';
+import * as ExpressMongoSanitize from 'express-mongo-sanitize';
 import sanitizeMiddleware from './middleware/sanitize.middleware.js';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        cors: true,
-    });
-    app.use(ExpressMongoSanitize());
-    app.use(sanitizeMiddleware);
-    await app.listen(process.env.PORT || 3000, () => {
-        if (process.env.NODE_ENV === 'production') {
-            console.log(`Server is running on production mode`);
-        } else {
-            console.log(`Server is running on dev mode`);
-        }
-    });
+	const app = await NestFactory.create(AppModule, {
+		cors: true,
+	});
+	app.use(ExpressMongoSanitize());
+	app.use(sanitizeMiddleware);
+	await app.listen(process.env.PORT || 3000, () => {
+		if (process.env.NODE_ENV === 'production') {
+			console.log(`Server is running on production mode`);
+		} else {
+			console.log(`Server is running on dev mode`);
+		}
+	});
 }
 bootstrap();
