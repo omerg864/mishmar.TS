53f2a8699add3f00c70086d669febb48aa969833
Omer Gaiznger <Omerg863@gmail.com>
2023-12-30 22:26:08 +0200

    pwa support

--- server/dist/main.js
+++ server/dist/main.js
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const express_mongo_sanitize_1 = require("express-mongo-sanitize");
+const ExpressMongoSanitize = require("express-mongo-sanitize");
 const sanitize_middleware_js_1 = require("./middleware/sanitize.middleware.js");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, {
         cors: true,
     });
-    app.use((0, express_mongo_sanitize_1.default)());
+    app.use(ExpressMongoSanitize());
     app.use(sanitize_middleware_js_1.default);
     await app.listen(process.env.PORT || 3000, () => {
         if (process.env.NODE_ENV === 'production') {
