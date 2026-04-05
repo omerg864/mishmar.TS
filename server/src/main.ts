e06db933f03146393a90355620a256927f4a64b8
Omer Gaiznger <Omerg863@gmail.com>
2026-04-06 00:05:44 +0300

    deploy

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,27 +1,36 @@
-/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import ExpressMongoSanitize from 'express-mongo-sanitize';
 import sanitizeMiddleware from './middleware/sanitize.middleware.js';
 import cookieParser from 'cookie-parser';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-async function bootstrap() {
-	const app = await NestFactory.create(AppModule, {
-		cors: {
-			origin: process.env.SITE_ADDRESS,
-			credentials: true,
-		},
-		logger: ['error', 'warn', 'log'],
-	});
-	app.use(ExpressMongoSanitize());
-	app.use(sanitizeMiddleware);
-	app.use(cookieParser());
-	await app.listen(process.env.PORT || 3000, () => {
-		if (process.env.NODE_ENV === 'production') {
-			console.log(`Server is running on production mode`);
-		} else {
-			console.log(`Server is running on dev mode`);
-		}
-	});
-}
+const server = express();
+
+export const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server), {
+    cors: {
+      origin: process.env.SITE_ADDRESS,
+      credentials: true,
+    },
+    logger: ['error', 'warn', 'log'],
+  });
+  app.use(ExpressMongoSanitize());
+  app.use(sanitizeMiddleware);
+  app.use(cookieParser());
+  await app.init();
+};
+
 bootstrap();
+
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
+    console.log(`Server is running on dev mode on port ${port}`);
+  });
+}
+
+export default server;
+
+
